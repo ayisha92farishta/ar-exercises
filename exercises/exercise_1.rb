7b54c8943faacd3a6525ba3adb2043ff2c8662a7
ayisha92farishta <ayisha_farishta@yahoo.com>
2021-12-30 22:04:35 +0000

    Add comments

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,7 +4,7 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
-
+# equivalent to INSERT INTO  from SQL
 store = Store.create(
   name: "Burnaby",
   annual_revenue: 300000,
@@ -12,19 +12,21 @@ store = Store.create(
   womens_apparel: true,
   )
 
-  store = Store.create(
+store = Store.create(
   name: "Richmond",
   annual_revenue: 1260000,
   mens_apparel: false,
   womens_apparel: true,
   )
 
-  store = Store.create(
-    name: "Gastown",
-    annual_revenue: 190000,
-    mens_apparel: true,
-    womens_apparel: false,
-    )
+store = Store.create(
+  name: "Gastown",
+  annual_revenue: 190000,
+  mens_apparel: true,
+  womens_apparel: false,
+  )
+
+#prints out the number of rows inside the table 'Store'  
 puts Store.all.count
 
 
