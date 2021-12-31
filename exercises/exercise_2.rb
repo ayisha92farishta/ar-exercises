a87809acc4f6e4b4416ecc71838da622451c3cf7
ayisha92farishta <ayisha_farishta@yahoo.com>
2021-12-31 04:59:50 +0000

    Minor format

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,15 +5,18 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
-
+#save sores info in instance variables
 @store1 = Store.first
 @store2 = Store.second
 
+#prints out names to check
 puts @store1.name
 puts @store2.name
 
+# edits name of store 1
 @store1.name = 'Hijabiana'
 @store1.save
 
+#prints out names to check
 puts @store1.name
 puts @store2.name
