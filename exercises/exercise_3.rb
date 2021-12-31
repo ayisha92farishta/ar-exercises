a9da7978c284042a84d4ced353c2d09ef38acc28
ayisha92farishta <ayisha_farishta@yahoo.com>
2021-12-31 05:03:15 +0000

    Delete store 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,14 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+#saving store 3 in instance variable
+@store3 = Store.third
+
+puts @store3.name
+
+#delete store 3
+@store3.destroy
+
+#prints out the number of rows inside the table 'Store' to check if the store is deleted
+puts Store.all.count
